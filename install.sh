242a6036b95178f63144dedd70f05889e367fbbe
presk0 <presk0@NICO.lan>
2024-01-13 11:29:34 +0100

    machine install mofified

--- install.sh
+++ install.sh
@@ -1,7 +1,22 @@
 #@/usr/bin/bash
 
-cp -r .oh-my-zsh .vim .zshrc .zshrc.pre-oh-my-zsh ..
+cp -r .vim .bashrc .bash_aliases ..
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo apt install vim -y
+sudo apt install gcc -y
+sudo apt install make -y
+sudo apt install pip -y
+sudo apt install vlc -y
+sudo apt install curl -y
+sudo apt install python3.11-venv -y
+#installation de whisper
+sudo apt install ffmpeg -y
+python3 -m venv ~/.env_whisper
+source ~/.env_whisper/bin/activate
+mkdir pip_cache
+TMP=$HOME/pip_cache/ ; TMPDIR=$TMP pip install --cache-dir=$TMP openai-whisper
+rm -rf ~/pip_cache/
+source ~/.bashrc
+# fin installation de whisper
