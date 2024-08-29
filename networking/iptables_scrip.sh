2b86ba4d67ef6ce4d2815f20a71d6e2989ae5977
presk0 <contact@presko.info>
2024-08-29 21:39:59 +0200

    passwd bug ? + iptables_script + yesccript regex

--- networking/iptables_scrip.sh
+++ networking/iptables_scrip.sh
@@ -1,54 +1,63 @@
 #!/bin/bash
-#if [ -n "$1" ]; then
-#	IP_LIST="$1"
-#else
-#	echo "missing IP list file"
-#	exit 0
-#fi
-##SIMPLE_RULES="~/machine/networking/simple_firewall.rules"
-#SIMPLE_RULES="/etc/iptables/simple_firewall.rules"
+
+ARGPARSE_DESCRIPTION="Configure your firewall from iptables"      # this is optional
+source $(dirname $0)/argparse.bash || exit 1
+argparse "$@" <<EOF || exit 1
+parser.add_argument('-f', '--file_in', help='ip list file (can contain CIDR)')
+parser.add_argument('-r', '--reset', action=argparse.BooleanOptionalAction, default=True, help='reset the rules')
+parser.add_argument('-s', '--simple', action=argparse.BooleanOptionalAction, default=True, help='set the rules as /etc/iptables/simple_firewall.rules')
+parser.add_argument('-l', '--log', action=argparse.BooleanOptionalAction, default=True, help='use -j LOG rule')
+parser.add_argument('-d', '--drop', action=argparse.BooleanOptionalAction, default=True, help='use -j DROP rule')
+parser.add_argument('-u', '--update', action=argparse.BooleanOptionalAction, default=True, help='save to /etc/iptables folder with iptables-save')
+EOF
+
 IPTABLES_FILE="/etc/iptables/iptables.rules"
 
-## from https://wiki.archlinux.org/title/Iptables
-## reset rules
-iptables -F
-iptables -X
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
-iptables -t raw -F
-iptables -t raw -X
-iptables -t security -F
-iptables -t security -X
-iptables -P INPUT ACCEPT
-iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
-#echo coucou 1
+if [ -n "$RESET" ]; then
+    ## from https://wiki.archlinux.org/title/Iptables
+    ## reset rules
+    iptables -F
+    iptables -X
+    iptables -t nat -F
+    iptables -t nat -X
+    iptables -t mangle -F
+    iptables -t mangle -X
+    iptables -t raw -F
+    iptables -t raw -X
+    iptables -t security -F
+    iptables -t security -X
+    iptables -P INPUT ACCEPT
+    iptables -P FORWARD DROP
+    iptables -P OUTPUT ACCEPT
+fi
 
-# Définir les politiques par défaut
-sudo iptables -P INPUT DROP
-sudo iptables -P FORWARD DROP
-sudo iptables -P OUTPUT ACCEPT
+if [ -n "$SIMPLE" ]; then
+    # Définir les politiques par défaut
+    iptables -P INPUT DROP
+    iptables -P FORWARD DROP
+    iptables -P OUTPUT ACCEPT
+    # Ajouter les règles spécifiques
+    iptables -A INPUT -p icmp -j ACCEPT
+    iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables -A INPUT -i lo -j ACCEPT
+    iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+    iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+    iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+fi
 
-# Ajouter les règles spécifiques
-sudo iptables -A INPUT -p icmp -j ACCEPT
-sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
-sudo iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
-sudo iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+if [ -f "$FILE_IN" ]; then
+    for IP in $(cut -d\n -f1 $FILE_IN); do
+        if [ -n "$LOG" ] ; then
+            iptables -I INPUT -s $IP -j LOG --log-prefix "IPTables-INPUT-Dropped: " --log-level 4
+            iptables -I OUTPUT -d $IP -j LOG --log-prefix "IPTables-OUTPUT-Dropped: " --log-level 4
+        fi
+        if [ -n "$DROP" ] ; then
+            iptables -I INPUT -s $IP -j DROP -m comment --comment "banned IP going in"
+            iptables -I OUTPUT -d $IP -j DROP -m comment --comment "banned IP going out"
+        fi
+    done
+fi
 
-##/etc/iptables/simple_firewall.rules
-#cat $SIMPLE_RULES > $IPTABLES_FILE
-##
-#echo coucou 2
-#for IP in $(cut -d\n -f1 $IP_LIST); do
-#	iptables -I INPUT -s $IP -j LOG --log-prefix "IPTables-INPUT-Dropped: " --log-level 4
-#	iptables -I INPUT -s $IP -j DROP -m comment --comment "banned IP going in"
-#	iptables -I OUTPUT -d $IP -j LOG --log-prefix "IPTables-OUTPUT-Dropped: " --log-level 4
-#	iptables -I OUTPUT -d $IP -j DROP -m comment --comment "banned IP going out"
-#done
-#echo coucou 3
-#
-iptables-save > "$IPTABLES_FILE"
+if [ -n "$UPDATE" ] ; then
+    iptables-save > "$IPTABLES_FILE"
+fi
