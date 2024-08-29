6c501c9c56fb44158c0b8f8275c970aa9e116697
presk0 <contact@presko.info>
2024-08-29 21:39:59 +0200

    passwd bug ? + iptables_script + yesccript regex

--- .vim/templates/template.sh
+++ .vim/templates/template.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/bash
+
+ARGPARSE_DESCRIPTION="cleaning audio before transcription"      # this is optional
+source $(dirname $0)/argparse.bash || exit 1
+argparse "$@" <<EOF || exit 1
+parser.add_argument('-f', '--file_in', nargs='*',
+                    required=True, help='audio file in')
+parser.add_argument('-o', '--file_out', required=True,
+		    help='audiofile out')
+EOF
+echo "[cleaning audio] $FILE_IN"
