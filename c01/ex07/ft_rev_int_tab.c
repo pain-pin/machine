88f257491d694211dc4bacd6f906b789c5e8cd74
Nicolas Dionis <nidionis@k0r1p6.42mulhouse.fr>
2024-09-05 17:17:24 +0200

    c01 not tested

---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nidionis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 16:20:59 by nidionis          #+#    #+#             */
+/*   Updated: 2024/09/05 17:06:15 by nidionis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	tmp;
+
+	i = -1;
+	while(i++ < --size)
+	{
+		tmp = tab[i];
+		tab[i] = tab[size];
+		tab[size] = tmp;
+	}
+}
+/*
+int	main(int argc, char **argv)
+{
+	return (0);
+}
+*/
