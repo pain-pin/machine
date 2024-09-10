10fd21d79a0ab3456c70f1b118686b200a4db891
Nicolas Dionis <nidionis@k0r1p2.42mulhouse.fr>
2024-09-10 17:22:35 +0200

    c02 c03 ajourne

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nidionis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 16:20:59 by nidionis          #+#    #+#             */
-/*   Updated: 2024/09/10 10:55:24 by nidionis         ###   ########.fr       */
+/*   Updated: 2024/09/10 14:37:45 by nidionis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
+	if (!n)
+		return (0);
 	while (*s1 && --n)
 	{
 		if (*s1 != *s2)
