df4c86f117a0c51ad175b5d430c62abaa1f9bad4
Nicolas Dionis <nidionis@k0r4p4.42mulhouse.fr>
2024-09-12 15:26:53 +0200

    c08

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nidionis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 16:20:59 by nidionis          #+#    #+#             */
+/*   Updated: 2024/09/12 15:21:07 by nidionis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN
+# define FT_BOOLEAN
+
+# include <unistd.h>
+# include <stdio.h>
+
+typedef int	t_bool;
+
+void ft_putstr(char *str);
+t_bool ft_is_even(int nbr);
+
+#endif
