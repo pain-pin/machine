2bbcbd8d91a67c38128340b1a21d372818ab8d26
Nicolas Dionis <nidionis@k0r4p4.42mulhouse.fr>
2024-09-12 15:26:53 +0200

    c08

---
+++ c08/ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nidionis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 16:20:59 by nidionis          #+#    #+#             */
+/*   Updated: 2024/09/12 15:15:05 by nidionis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT
+# define FT
+
+void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
+
+#endif
