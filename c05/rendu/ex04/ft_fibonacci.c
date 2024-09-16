d6a10b5350c5f0990d4b1ed70514b00477fc72fd
Nicolas Dionis <nidionis@k0r4p4.42mulhouse.fr>
2024-09-16 11:14:23 +0200

    first try

---
+++ c05/rendu/ex04/ft_fibonacci.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nidionis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 16:20:59 by nidionis          #+#    #+#             */
+/*   Updated: 2024/09/11 17:36:41 by nidionis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (!index)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+/*
+int	main(int argc, char **argv)
+{
+	return (0);
+}
+*/
