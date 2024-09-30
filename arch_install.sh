be11c59a402a835190c846d088739b72bab8d017
presk0 <contact@presko.info>
2024-09-30 07:25:25 +0000

    machine install mofified

--- arch_install.sh
+++ arch_install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 MACHINE_DIR="$HOME/machine"
 
 bash $MACHINE_DIR/networking/iptables_script.sh
@@ -7,9 +13,8 @@ mv ~/.bashrc ~/.bashrc_original
 
 ln -s $MACHINE_DIR/.vim $MACHINE_DIR/.bashrc $MACHINE_DIR/.bash_aliases $MACHINE_DIR/.config $HOME
 
-
-
 pacman -Syu cherrytree
+pacman -S firefox
 pacman -S redshift
 pacman -S vlc
 pacman -S tcpdump
@@ -21,12 +26,16 @@ pacman -S syslog-ng
 pacman -S nmap
 pacman -S lldb
 pacman -S terminator
-pacman -S man
+pacman -S man-db
 pacman -S apropos
 pacman -S gdb
+mandb
 #pacman -S zram-generator
 #pacman -S cuda
 #pacman -S zsh
+
+git config --global user.email "contact@presko.info"
+git config --global user.name "presk0"
 
 #echo "zram-size = ram w* 2\ncompression-algorithm = zstd" >> /etc/systemd/zram-generator.conf
 
@@ -45,4 +54,4 @@ pacman -S gdb
 
 # log iptables dans /var/log/iptables.log au lieu de dmesg via syslog-ng
 #awk '/filter f_everything/ { print "#" $0; print "filter f_everything { level(debug..emerg) and not facility(auth, authpriv) and not filter(f_iptables); };" ; next } 1' /etc/syslog-ng/syslog-ng.conf > /tmp/tmp
-mv /tmp/tmp /etc/syslog-ng/syslog-ng.conf
+#mv /tmp/tmp /etc/syslog-ng/syslog-ng.conf
