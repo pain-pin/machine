90f9aa888a7a2d8e8927e2b794d7db7fc758a179
N <n@localhost.localdomain>
2025-03-05 01:23:07 +0100

    flatpack settings

--- config_install.sh
+++ config_install.sh
@@ -10,5 +10,6 @@ for F in $FILES_TO_LINK; do
 	ln -s $F $SUDO_HOME/$BASENAME
 done
 
+git clone https://github.com/Exafunction/codeium.vim ~/.vim/pack/Exafunction/start/codeium.vim
 #mkdir -p ~/.config/nvim
 #ln -s ~/.vimrc ~/.config/nvim/init.vim
