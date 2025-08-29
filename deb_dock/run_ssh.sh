6d1fd30cc90d17db002f2865af26fd54c7de4799
Nicolas Dionis <nidionis@k1r4p7.42mulhouse.fr>
2025-08-29 20:27:08 +0200

    deb_dock/run_ssh.sh start to look like something

--- deb_dock/run_ssh.sh
+++ deb_dock/run_ssh.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/bash
 
-# Usage: ./run_ssh.sh 2222
 SSH_PORT=${1:-2222}
+NAME=${2:-"debian_ssh"}
+DOCKER_ID=$(docker ps | awk '/ssh/ {print $1}')
 
-# Build avec ARG
-docker build --build-arg SSH_PORT=$SSH_PORT -t debian_ssh .
+docker stop $DOCKER_ID
+docker rm $DOCKER_ID
+docker build --build-arg SSH_PORT=$SSH_PORT -t $NAME .
 
-# Run avec mapping dynamique
 docker run -d \
   -p ${SSH_PORT}:${SSH_PORT} \
   -e SSH_PORT=${SSH_PORT} \
   -v $(pwd)/root-only:/srv/root-only \
   -v $(pwd)/shared:/srv/shared \
   -v $(pwd)/bin:/srv/bin \
-  --name debian_custom debian_ssh
+  --name debian_custom $NAME
 
