74d982dbdc235ff717dc356a94f09294c77d2c29
Nicolas Dionis <nidionis@k1r4p13.42mulhouse.fr>
2025-08-30 18:36:21 +0200

    Docker fair enough

--- bin/42_install.sh
+++ bin/42_install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
+HOME_SKEL="~/machine/deb_dock/home_skel"
+
 cd
 ln -s machine/bin .
-for F in ~/machine/dotfiles/.[a-z]* ; do
+for F in $HOME_SKEL/.[a-z]* ; do
 	DOTFILE="$(basename $F)"
 	rm $DOTFILE
 	ln -s $F $DOTFILE
--- deb_dock/home_skel/.vim
+++
@@ -1,1 +0,0 @@
-/home/nidionis/machine/dotfiles/.vim
