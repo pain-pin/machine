5e576045f223c0a887b64a9da5103a12bdd59be4
arche <github@presko.info>
2025-10-12 11:12:43 +0200

    ../install/pacman.sh modem mnager

--- install/pacman.sh
+++ install/pacman.sh
@@ -30,10 +30,11 @@ pacman --noconfirm -S makepkg
 pacman --noconfirm -S tree
 pacman --noconfirm -S vim
 pacman --noconfirm -S iwctl
+pacman --noconfirm -S pciutils
 mandb #rend possible la commande apropos
 
 #git clone https://github.com/Exafunction/codeium.vim ~/.vim/pack/Exafunction/start/codeium.vim
-
+iwctl
 sudo -i -u $USER bash << EOF
 
 git config --global user.email "contact@presko.info"
