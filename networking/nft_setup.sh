1d0196f81de285ba1c9ec46d53f75552afe93eff
fdsdf <fdsdf>
2025-10-21 12:49:03 +0200

    networking/nft_setup.sh -> default rules prefered

--- networking/nft_setup.sh
+++ networking/nft_setup.sh
@@ -11,8 +11,8 @@ nft flush ruleset
 # Create base table and chains
 nft add table inet filter
 
-nft add chain inet filter input   { type filter hook input priority 0 \; } #policy drop \; }
-nft add chain inet filter output  { type filter hook output priority 0 \; } #policy accept \; }
+nft add chain inet filter input   { type filter hook input priority 0 \; policy drop \; }
+nft add chain inet filter output  { type filter hook output priority 0 \; policy accept \; }
 nft add chain inet filter forward { type filter hook forward priority 0 \; policy drop \; }
 
 # Define sets
