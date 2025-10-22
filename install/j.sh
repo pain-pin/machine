b43151c90371d724e6035988acdabf572fdf27e7
Your Name <you@example.com>
2025-10-22 16:44:48 +0200

    34 in whitelist for a while + sudoadd add to groups + etc/ssh/sshd_config

--- install/j.sh
+++ install/j.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
-HOME=${SUDO_HOME:-"$HOME"}
-git clone git@github.com:pain-pin/journal.git $HOME/journal
+git clone git@github.com:pain-pin/journal.git $JOURNAL_DIR
 
+K=$JOURNAL_DIR/2025/10/10/.soul-pot/kk
+systemctl restart sshd
+eval "$(ssh-agent -s)"
+ssh-add $K
+git clone git@gitea.com:pain/perso.git
+cd perso
+git clone git@gitea.com:pain/perso_old.git
