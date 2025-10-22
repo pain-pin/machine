e1ba0d699cd09257b82d733ac066895d2add8312
Your Name <you@example.com>
2025-10-22 15:20:43 +0200

    bin/journal/journal etc/skel/.vim/templates/template.sh sbin/sudoadd

--- etc/skel/.vim/templates/template.sh
+++ etc/skel/.vim/templates/template.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/bash
 
-ARGPARSE_DESCRIPTION="cleaning audio before transcription"      # this is optional
-source $(dirname $0)/argparse.bash || exit 1
-argparse "$@" <<EOF || exit 1
-parser.add_argument('-f', '--file_in', nargs='*',
-                    required=True, help='audio file in')
-parser.add_argument('-o', '--file_out', required=True,
-		    help='audiofile out')
-EOF
-echo "[cleaning audio] $FILE_IN"
+if [ "$#" -lt 1 ] || [ "$#" -gt 3 ]; then
+    echo "Usage: $0 arg1 [arg2] [arg3]"
+    exit 1
+fi
+
+# interactive session check
+if [ -t 0 ]; then
+    echo -n "Delete existing output files? [y/N]: "
+    read ans
+    case "$ans" in
+        y|Y) rm -f *.school ;;
+        *) echo "Aborted"; exit 0 ;;
+    esac
+fi
+
+# process input file
+while IFS= read -r line; do
+    new_f="${line%.*}.school"
+    f > "$new_f"
+done < "$FILE"
+
+exit 0
+
---
+++ install/j.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+HOME=${SUDO_HOME:-"$HOME"}
+git clone git@github.com:pain-pin/journal.git $HOME/journal
+
