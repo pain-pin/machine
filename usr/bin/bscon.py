1caa09167c5d5bd9ad6f69ff586a3cee7735c097
Your Name <you@example.com>
2025-10-29 16:48:50 +0100

    usr/bin/bscon.py keep last user

--- usr/bin/bscon.py
+++ usr/bin/bscon.py
@@ -6,20 +6,35 @@ import argparse
 DEFAULT_DOMAIN="bsky.social"
 DEFAULT_DIR="/tmp/bs"
 
+def get_last(tokendir=DEFAULT_DIR):
+    with open(tokendir + "last", "r") as f:
+        last = f.read()
+    return last
+
+def set_last(full_name, tokendir=DEFAULT_DIR):
+    with open(tokendir + "last", "w") as f:
+        f.write(full_name)
+
 def save_session(client, full_name, tokendir):
     session_string = client.export_session_string()
     with open(tokendir + full_name, "w") as f:
         f.write(session_string)
+    set_last(full_name)
 
 def get_session(client, full_name, tokendir):
+    print(full_name)
     with open(tokendir + full_name, "r") as f:
         session_string = f.read()
     client.login(session_string=session_string)
+    set_last(full_name)
     return client
 
-def connect(name, domain, passwd, token_dir):
+def connect(name=None, domain=DEFAULT_DOMAIN, passwd=None, token_dir=DEFAULT_DOMAIN):
     client = Client()
-    full_name = name + "." + domain
+    full_name = get_last()
+    if name:
+        full_name = name + "." + domain
+    print("fullname = ", full_name)
     try:
         client.login(full_name, passwd)
         save_session(client, full_name, token_dir)
@@ -38,7 +53,6 @@ def main():
 
     client = connect(args.name, args.domain, args.passwd, args.tokendir)
     print(f"connected")
-    client.send_post(text='Hello World!')
 
 if __name__ == "__main__":
     main()
