aa1449fbfa972bc543e13b888e565ccd4aec8d3c
Your Name <you@example.com>
2025-11-10 13:42:50 +0100

    tree_cpy.sh -> -l (link) option

--- install/tree_cpy.sh
+++ install/tree_cpy.sh
@@ -5,5 +5,5 @@ USR_DIR=${1:-"no_dir"}
 	echo "usage $0 usr_dir" && \
 	exit 1
 USR_DIR=$(realpath $USR_DIR)
-cp -a -p -r $USR_DIR/* /
-cp -a -p -r $USR_DIR/home/.* /home
+cp -l -p -r $USR_DIR/* /
+cp -l -p -r $USR_DIR/home/.* /home
