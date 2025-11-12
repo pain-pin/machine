4f2e4fe751dbfdf239c4ab78e41f5f91f78d1d17
Your Name <you@example.com>
2025-11-12 14:05:32 +0100

    librewolf/docker-compose.yml librewolf/run_librewolf.sh

---
+++ dock/librewolf/run_librewolf.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# https://belginux.com/installer-librewolf-avec-docker/
+#
+docker compose up -d
+
+# Rendez-vous sur l'ip:# port, 
+# suivant l'IP de votre serveur local et du port choisi, 
+# 3000 par défaut:
+# 
+# http://ip:3004/
+# 
+# En HTTPS:
+# 
+# https://ip:3001/
