ba8421be79a86facb919138513690d3f62ceea2b
root@terrarch2.5 <contact@presko.info>
2025-11-12 20:37:00 +0100

    ../install/pacman.sh firejail added

--- install/pacman.sh
+++ install/pacman.sh
@@ -9,34 +9,28 @@ pacman --noconfirm -Syu makepkg
 pacman --noconfirm -Syu nftables
 pacman --noconfirm -Syu bc
 pacman --noconfirm -Syu ctags
-#pacman --noconfirm -Syu firefox
+pacman --noconfirm -Syu firefox
+pacman --noconfirm -Syu firejail
 pacman --noconfirm -Syu unzip
-#pacman --noconfirm -Syu redshift
-#pacman --noconfirm -Syu vlc
 pacman --noconfirm -Syu tcpdump
 pacman --noconfirm -Syu moreutils
 pacman --noconfirm -Syu net-tools
 pacman --noconfirm -Syu make
-#pacman --noconfirm -Syu whois
-#pacman --noconfirm -Syu nmap
-#pacman --noconfirm -Syu lldb
-#pacman --noconfirm -Syu terminator
+pacman --noconfirm -Syu whois
+pacman --noconfirm -Syu nmap
 pacman --noconfirm -Syu man-db
 pacman --noconfirm -Syu apropos
-#pacman --noconfirm -Syu gdb
 pacman --noconfirm -Syu docker
-#pacman --noconfirm -Syu go
 pacman --noconfirm -Syu makepkg
 pacman --noconfirm -Syu tree
 pacman --noconfirm -Syu vim
 pacman --noconfirm -Syu iwctl
 pacman --noconfirm -Syu pciutils
-#pacman --noconfirm -Syu base-devel
 pacman --noconfirm -Syu git-filter-repo
 pacman --noconfirm -Syu xorg-setxkbmap
 pacman --noconfirm -Syu xorg-xhost
 mandb #rend possible la commande apropos
-#
+
 ##git clone https://github.com/Exafunction/codeium.vim ~/.vim/pack/Exafunction/start/codeium.vim
 #sudo -i -u $SUDO_USER bash << EOF
 #
@@ -44,9 +38,9 @@ mandb #rend possible la commande apropos
 #git config --global user.name "prsko_$HOST"
 #EOF
 
-su $SUDO_USER
-git clone https://aur.archlinux.org/yay.git
-cd yay/
-makepkg -si
-cd ..
-rm -rf yay
+#su $SUDO_USER
+#git clone https://aur.archlinux.org/yay.git
+#cd yay/
+#makepkg -si
+#cd ..
+#rm -rf yay
