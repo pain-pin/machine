8baf180f248d07e0bdb10d07c22ad81c08355a7d
Your Name <you@example.com>
2025-11-16 14:21:06 +0100

    bscon.py

--- usr/bin/bscon.py
+++ usr/bin/bscon.py
@@ -5,7 +5,7 @@ import os
 
 DEFAULT_DOMAIN = "bsky.social"
 DEFAULT_NAME = "ni-bot"
-DEFAULT_DIR = "/tmp/bs"
+DEFAULT_DIR = "~/.config/bs"
 
 def path(tokendir, name="ni-bot"):
     return os.path.join(tokendir, name)
