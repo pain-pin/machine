38af57cd9e45b68044e4aa74e664416d472474ff
terrarch2.5 <>
2025-11-19 20:03:10 +0100

    modified:   net/blacklist.txt modified:   net/whitelist.txt modified:   usr/etc/env deleted:    usr/etc/systemd/system/tcpd-bkup.service modified:   usr/etc/systemd/system/tcpd-bkup.sh deleted:    usr/etc/systemd/system/tcpd-bkup.timer modified:   usr/etc/systemd/system/tcpd.sh modified:   usr/home/.config/qtile/config.py

--- usr/etc/systemd/system/tcpd-bkup.sh
+++ usr/etc/systemd/system/tcpd-bkup.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/bash
+#run from crontab
 
 source /home/.bashrc
 
 . refresh_time
-grepip $LOG_CONN > $LOG_CONN_DIR/$DATE_$TIME.conn
+
+STATUS=ronron
+if [ -f $NET_STATUS ] ; then
+	STATUS=$(cat $NET_STATUS)
+fi
+
+DIR=$LOG_CONN_DIR/$STATUS
+mkdir -p $DIR
+grepip $LOG_CONN > $DIR/${DATE}_${TIME}.conn
 mv -f $LOG_CONN $LOG_CONN_DIR/prev.log
+systemctl restart tcpd
 
 exit 0
 
--- usr/home/.config/qtile/config.py
+++ usr/home/.config/qtile/config.py
@@ -180,7 +180,41 @@ screens = [
         # By default we handle these events delayed to already improve performance, however your system might still be struggling
         # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
         # x11_drag_polling_rate = 60,
+        x=1920,y=0
+        width=1600, height=900
     ),
+    Screen = [
+        bottom=bar.Bar(
+            [
+                widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        "launch": ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                # widget.StatusNotifier(),
+                widget.Systray(),
+                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+                widget.QuickExit(),
+            ],
+            24,
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+        background="#000000",
+        wallpaper=logo,
+        wallpaper_mode="center",
+        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
+        # By default we handle these events delayed to already improve performance, however your system might still be struggling
+        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
+        # x11_drag_polling_rate = 60,
+        x=0,y=0
+        width=1920, height=1080
+        ]
 ]
 
 # Drag floating layouts.
